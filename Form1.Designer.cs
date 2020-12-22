21415c388cf4e9d837e378b1d04102dd7be3e65a
luizfAM4 <luiz.felipe@am4.com.br>
2020-12-22 12:04:12 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RenomearArquivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.btnRenomear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(65, 56);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(171, 20);
+            this.txtArea.TabIndex = 3;
+            // 
+            // btnRenomear
+            // 
+            this.btnRenomear.Location = new System.Drawing.Point(65, 130);
+            this.btnRenomear.Name = "btnRenomear";
+            this.btnRenomear.Size = new System.Drawing.Size(118, 50);
+            this.btnRenomear.TabIndex = 6;
+            this.btnRenomear.Text = "Renomear";
+            this.btnRenomear.UseVisualStyleBackColor = true;
+            this.btnRenomear.Click += new System.EventHandler(this.btnRenomear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Diretório";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Área";
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Location = new System.Drawing.Point(65, 18);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(171, 20);
+            this.txtDiretorio.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data";
+            // 
+            // txtData
+            // 
+            this.txtData.Enabled = false;
+            this.txtData.Location = new System.Drawing.Point(65, 91);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(171, 20);
+            this.txtData.TabIndex = 5;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(251, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 195);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtDiretorio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRenomear);
+            this.Controls.Add(this.txtArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Renomear Arquivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Button btnRenomear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenomearArquivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void btnRenomear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dir = txtDiretorio.Text;
+                string d = dir + "/";
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                FileInfo[] arquivos = dirInfo.GetFiles().OrderBy(p=>p.CreationTime).ToArray();
+                string area = txtArea.Text;
+                string data = txtData.Text;
+
+                for (int i = 0; i < arquivos.Length; i++)
+                {
+                    string numeracao = (i + 1).ToString();
+                    string nomeAntigo;
+                    string novoNome;
+                    string extensao;
+                    int value = i + 1;
+
+                    if (numeracao.Length == 1)
+                    {                      
+                        nomeAntigo = d + arquivos[i].Name;
+                        extensao = arquivos[i].Extension;
+                        novoNome = d + "0" + value.ToString() + "-" + area + "-"+ data +".png";
+                    }
+                    else
+                    {
+                        nomeAntigo = d + arquivos[i].Name;
+                        extensao = arquivos[i].Extension;
+                        novoNome = d + value.ToString() + "-" + area + "-" + data + ".png";
+                    }
+                    File.Move(nomeAntigo, novoNome);                   
+                }
+                MessageBox.Show( "Arquivos Renomeados com sucesso!", "Renomear", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimparCampos();
+            }catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            var d1 = monthCalendar1.SelectionStart.ToString("dd-MM-yyyy");
+            txtData.Text = d1;
+        }
+
+        public void LimparCampos()
+        {
+            txtArea.Text = "";
+            txtData.Text = "";
+            txtDiretorio.Text = "";
+        }
+    }
+}
